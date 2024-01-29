e0840a9418c5749dfec2308bb7943e9800d3bec4
DeepuRaj184 <deepura27@gmail.com>
2024-01-29 18:12:11 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Dropdown from "./Dropdown";
+import { DropContext } from "./dropcontext";
+import Dropdisplay from "./Dropdisplay";
+
+function App() {
+  return (
+    <DropContext>
+      <Dropdown />
+      <Dropdisplay />
+    </DropContext>
+    
+  );
+}
+
+export default App;
---
+++ src/Dropdown.jsx
@@ -0,0 +1,15 @@
+import { cloneElement } from "react"
+import styles from "./css/Dropdown.module.css"
+import { Dropvalue } from "./dropcontext"
+
+function Dropdown(){
+    const {hover}= Dropvalue()
+    return(
+        <>
+            <button onMouseOver={(e)=>hover(e)} className={styles.button}>Select</button>
+        </> 
+        
+    )
+}
+
+export default Dropdown;
